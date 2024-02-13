0f4691bd093d016eaee5f529f244b97b547244b8
Jacob Liddle <u1070198@utah.edu>
2024-02-13 11:05:33 -0700

    Initial commit, including main and test class files.

---
+++ SerialTest/SerialComPort.h
@@ -0,0 +1,51 @@
+//IMPORTANT!!! This class is a copy of the UClass that I'm using for testing in straight cpp
+
+
+// Parts of the code for this are based off of this plugin: https://github.com/videofeedback/Unreal_Engine_SerialCOM_Plugin
+// Other resources used include the following:
+// Windows API documentation
+// https://learn.microsoft.com/en-us/windows/win32/devio/configuring-a-communications-resource
+// https://stackoverflow.com/questions/15794422/serial-port-rs-232-connection-in-c
+// http://hdrlab.org.nz/articles/windows-development/a-c-class-for-controlling-a-comm-port-in-windows/
+
+#pragma once
+
+#define FLUSH_BUFFER_SIZE 1024
+
+#include <Windows.h>
+#include <string>
+
+
+
+/**
+ * The Serial Port data type to be used to manage opened serial ports
+ */
+class USerialComPort
+{
+
+protected:
+	HANDLE hCom;
+	int portNumber;
+	//I'll need overlapped structs later for async?
+
+public:
+	USerialComPort();
+	~USerialComPort();
+
+	bool IsOpened();
+
+	bool Open(int port);
+
+	void Close();
+
+	/*
+	* This Function doesn't actually read a line currently, it reads the number of bytes that I want it to read.
+	*/
+	std::string ReadLine();
+
+	void Flush();
+
+	//Later we may want write functions
+
+	static USerialComPort* OpenComPort(bool& bOpened, int portNumber);
+};
---
+++ SerialTest/SerialTest.cpp
@@ -0,0 +1,123 @@
+
+#include <iostream>
+
+#include <Windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+#include "SerialComPort.h"
+
+void PrintCommState(DCB dcb)
+{
+    //  Print some of the DCB structure values
+    _tprintf(TEXT("\nBaudRate = %d, ByteSize = %d, Parity = %d, StopBits = %d\n"),
+        dcb.BaudRate,
+        dcb.ByteSize,
+        dcb.Parity,
+        dcb.StopBits);
+}
+
+void SerialTest()
+{
+    DCB dcb;
+    HANDLE hCom;
+    bool fSuccess;
+    const wchar_t* pcCommPort = TEXT("COM7"); //Our current port
+
+    //Open a handle to our com port
+    hCom = CreateFile(pcCommPort,
+        GENERIC_READ | GENERIC_WRITE,
+        0, //No sharing
+        NULL, //Default security attributes
+        OPEN_EXISTING, //Must use this
+        0, //not overlapped I/O     TO BE ASYNC WE NEED OVERLAPPED
+        NULL); //Template must be NULL for comm devices
+
+    if (hCom == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateFile failed with error %d.\n", GetLastError());
+        return;
+    }
+
+    // Initialize the DCB structure.
+    SecureZeroMemory(&dcb, sizeof(DCB));
+    dcb.DCBlength = sizeof(DCB);
+
+    // Build on the current configuration by first retrieving all current settings.
+    fSuccess = GetCommState(hCom, &dcb);
+    if (!fSuccess)
+    {
+        //  Handle the error.
+        printf("GetCommState failed with error %d.\n", GetLastError());
+        return;
+    }
+
+    //  Fill in some DCB values and set the com state: 
+    //  9600 bps, 8 data bits, no parity, and 1 stop bit.
+    dcb.BaudRate = CBR_9600;
+    dcb.ByteSize = 8;
+    dcb.StopBits = ONESTOPBIT;
+    dcb.Parity = NOPARITY;
+
+    fSuccess = SetCommState(hCom, &dcb);
+
+    if (!fSuccess)
+    {
+        printf("SetCommState failed with error %d.\n", GetLastError());
+        return;
+    }
+
+    PrintCommState(dcb);
+
+    COMMTIMEOUTS timeout;
+    SecureZeroMemory(&timeout, sizeof(COMMTIMEOUTS));
+
+    timeout.ReadIntervalTimeout = 1;
+    timeout.ReadTotalTimeoutMultiplier = 0; //This says that the total timeout is X ms per byte
+    timeout.ReadTotalTimeoutConstant = 0; //This is added to the multiplier
+    SetCommTimeouts(hCom, &timeout);
+
+    _tprintf(TEXT("Serial port %s successfully reconfigured.\n"), pcCommPort);
+
+    LPVOID lpBuffer = calloc(8, sizeof(char));
+    DWORD numBytes = 7; //If we output a line of 5 chars, the string also has a newline char and a null terminator, so we need 7
+    LPDWORD lpBytesRead = static_cast<LPDWORD>(malloc(sizeof(DWORD))); 
+    bool rSuccess = true;
+
+    //read loop
+    while (rSuccess)
+    {
+        rSuccess = ReadFile(hCom, lpBuffer, numBytes, lpBytesRead, NULL);
+        std::cout << static_cast<char*>(lpBuffer) << std::endl;
+    }
+
+    CloseHandle(hCom);
+}
+
+void SerialClassTest()
+{
+    bool opened;
+    USerialComPort* port;
+    for (int i = 7; i <= 8; i++)
+    {
+        port = USerialComPort::OpenComPort(opened, i);
+
+        if (opened)
+        {
+            printf("Port %d opened successfully!\n", i);
+            break;
+        }
+        printf("Port %d didn't open\n", i);
+    }
+
+    while (true)
+        printf(port->ReadLine().c_str());
+}
+
+int main()
+{
+    //Select which of these to use based on if I'm testing the communication directly or testing the class
+    SerialClassTest();
+
+    SerialTest();
+}
